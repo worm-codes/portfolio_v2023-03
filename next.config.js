7a60c95be2f454e9a36931394fdc6807d9893077
oguzhan.cevik <oguzhan@90pixel.com>
2023-02-19 17:08:39 +0300

    fix style components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  compiler: {
+    // Enables the styled-components SWC transform
+    styledComponents: true,
+  },
 
-module.exports = nextConfig
+  images: {
+    domains: ['unsplash.com'],
+  },
+};
+
+module.exports = nextConfig;
